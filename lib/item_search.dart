1f9ed3506be22b8d2cbb121d05275b89b6a5e081
Shubbair <hussainsalih.github@gmail.com>
2021-08-17 13:50:51 +0300

    update main

--- lib/item_search.dart
+++ lib/item_search.dart
@@ -5,8 +5,44 @@ class ItemSearch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Gallery'),
+        centerTitle: true,
+        backgroundColor: Colors.orangeAccent[700],
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                    width: MediaQuery.of(context).size.width * 0.90,
+                    margin: EdgeInsets.only(top: 15, bottom: 15),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.grey[600],
+                        ),
+                        // suffixIcon: Icon(Icons.ac_unit),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(6)),
+                          borderSide: BorderSide.none,
+                          //borderSide: const BorderSide(),
+                        ),
+                        hintStyle: TextStyle(
+                            color: Colors.grey[500], fontFamily: "WorkSansLight"),
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                        hintText: 'Search',
+                      ),
+                    ),
+                  ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
