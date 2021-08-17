519e862b5f4814d2ecb4e13f6e46e99aad2b079b
Shubbair <hussainsalih.github@gmail.com>
2021-08-17 13:18:45 +0300

    first comment

---
+++ lib/image_preview.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ImagePreview extends StatelessWidget {
+  const ImagePreview({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
---
+++ lib/item_search.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ItemSearch extends StatelessWidget {
+  const ItemSearch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
