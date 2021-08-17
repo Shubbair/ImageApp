994eeb4d32e15108a248f1e61a5790b56f265128
Shubbair <hussainsalih.github@gmail.com>
2021-08-17 14:25:46 +0300

    solve scroll issue

--- lib/item_search.dart
+++ lib/item_search.dart
@@ -6,56 +6,108 @@ class ItemSearch extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         title: Text('Image Gallery'),
         centerTitle: true,
         backgroundColor: Colors.orangeAccent[700],
       ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
             children: [
-              Container(
-                width: MediaQuery.of(context).size.width * 0.90,
-                margin: EdgeInsets.only(top: 15, bottom: 15),
-                child: TextFormField(
-                  onFieldSubmitted:(text){
-                     print(text);
-                  },
-                  decoration: InputDecoration(
-                    
-                    prefixIcon: Icon(
-                      Icons.search,
-                      color: Colors.grey[600],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.90,
+                    margin: EdgeInsets.only(top: 15, bottom: 15),
+                    child: TextFormField(
+                      onFieldSubmitted:(text){
+                         print(text);
+                      },
+                      decoration: InputDecoration(
+                        
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.grey[600],
+                        ),
+                        // suffixIcon: Icon(Icons.ac_unit),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(6)),
+                          borderSide: BorderSide.none,
+                          //borderSide: const BorderSide(),
+                        ),
+                        hintStyle: TextStyle(
+                            color: Colors.grey[500], fontFamily: "WorkSansLight"),
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                        hintText: 'Search',
+                      ),
                     ),
-                    // suffixIcon: Icon(Icons.ac_unit),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(6)),
-                      borderSide: BorderSide.none,
-                      //borderSide: const BorderSide(),
-                    ),
-                    hintStyle: TextStyle(
-                        color: Colors.grey[500], fontFamily: "WorkSansLight"),
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    hintText: 'Search',
                   ),
-                ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin : EdgeInsets.only(top:16),
+                    width: 175,
+                    height : 200,
+                    color: Colors.red,
+                    child: Text('data'),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 16,top:16),
+                    width: 175,
+                    height : 200,
+                    color: Colors.red,
+                    child: Text('data'),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top:16),
+                    width: 175,
+                    height : 200,
+                    color: Colors.red,
+                    child: Text('data'),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 16,top:16),
+                    width: 175,
+                    height : 200,
+                    color: Colors.red,
+                    child: Text('data'),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin : EdgeInsets.only(top:16),
+                    width: 175,
+                    height : 200,
+                    color: Colors.red,
+                    child: Text('data'),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 16,top:16),
+                    width: 175,
+                    height : 200,
+                    color: Colors.red,
+                    child: Text('data'),
+                  ),
+                ],
               ),
             ],
           ),
-          Row(
-            children: [
-              Container(
-                width: 200,
-                height : 50,
-                color: Colors.red,
-                child: Text('data'),
-              )
-            ],
-          )
-        ],
+        ),
       ),
     );
   }
