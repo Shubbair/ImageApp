fdd7642343887d73c665774dcec2cce18c983e92
Shubbair <hussainsalih.github@gmail.com>
2021-08-20 19:35:26 +0300

    Upload files

---
+++ lib/image.dart
@@ -0,0 +1,8 @@
+class ImageStruct {
+  final String path;
+  final double marginTop;
+  final double marginLeft;
+  final double marginBottom;
+
+  ImageStruct({required this.path,required this.marginLeft,required this.marginTop, required this.marginBottom});
+}
