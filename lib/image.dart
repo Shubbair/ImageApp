d10abc326c7dc898217f804ba4dc7a16158d6396
Hussain <hussainsalih.github@gmail.com>
2021-08-22 17:33:34 +0300

    Delete lib directory

--- lib/image.dart
+++
@@ -1,8 +0,0 @@
-class ImageStruct {
-  final String path;
-  final double marginTop;
-  final double marginLeft;
-  final double marginBottom;
-
-  ImageStruct({required this.path,required this.marginLeft,required this.marginTop, required this.marginBottom});
-}
